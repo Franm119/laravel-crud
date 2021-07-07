645ce385e708ec40149284872ff18a77cb100a3c
Francisco Martillo <franciscom119@gmail.com>
2021-07-07 14:13:57 -0400

    Identificamos los campo que tenemos en la bd

--- app/Personal/Personal.php
+++ app/Personal/Personal.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personal extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['names', 'last_name', 'sex', 'birthday'];
 }
