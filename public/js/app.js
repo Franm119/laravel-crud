f14c1eef10beb9e1dd09ab0bad87866abd0dc0f7
Francisco Martillo <franciscom119@gmail.com>
2021-10-16 22:32:17 -0400

    Compilación y ejecución.
    Se modificaron archivos después de compilar.

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Francisco Martillo\Desktop\laravel-crud\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Francisco Martillo\Desktop\laravel-crud\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Franyer\Documents\Fco\development\laravel-crud\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Franyer\Documents\Fco\development\laravel-crud\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
