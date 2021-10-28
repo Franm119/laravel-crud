9fb680d0457dd8702bb50ef583b57b8f919a587e
Francisco Martillo <franciscom119@gmail.com>
2021-10-28 15:22:20 -0400

    Página principal
    Página pricipal de crud

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="card text-center">
+                <div class="card-header">
+                    CRUD
+                    </div>
+                    <div class="card-body">
+                    <h5 class="card-title">CRUD-HOME</h5>
+                    <p class="card-text">Laravel 7.30.4 y Bootstrap 4.3.1.</p>
+                </div>
+                <div class="card-footer text-muted">
+                    LARAVEL
+                </div>
+        </div>
+    </div>
+@endsection
