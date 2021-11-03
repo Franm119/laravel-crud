0d16522f55a031818d8da3045dff0a92b92f1980
Francisco Martillo <franciscom119@gmail.com>
2021-11-02 22:37:47 -0400

    Métodos create y store
    Método que permite create personal

--- app/Http/Controllers/Personal/PersonalController.php
+++ app/Http/Controllers/Personal/PersonalController.php
@@ -18,4 +18,25 @@ class PersonalController extends Controller
         $personal = Personal::orderBy('id')->paginate(4);
         return view('personal.index', compact('personal')) ;
     }
+
+    /**
+     * Encargado de recolectar los datos, para almacenarlos en un registro nuevo.
+     *
+     * @return void
+     */
+    public function create(){
+        return view('personal.create');
+    }
+
+    /**
+     * Se crea un registro en especifico, los datos provienen del método create.
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(PersonalRequest $request){
+        Personal::create($request->validated());
+        return redirect()->route('personal.index');
+    }
+    
 }
