4b1541e68ebf67bd4226d18ecf0a28b828ce8bda
Francisco Martillo <franciscom119@gmail.com>
2021-11-03 11:37:37 -0400

    Footer
    Footer de crud.

---
+++ resources/views/personal/fragment/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-while text-center text-black-50 py-3 shadow" >
+  <div class="container">
+    <span class="text-muted">{{ config('app.name') }} | Copyright @ {{ date('Y') }}</span>
+  </div>
+</footer>
